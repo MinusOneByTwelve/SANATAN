c2624e1546ca769325db69e7a4dad5497459a611
MinusOneByTwelve <50650979+MinusOneByTwelve@users.noreply.github.com>
2024-04-23 17:32:51 +0530

    Create FirstRun.sh

---
+++ FirstRun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+
+sudo mv Resources/* /opt/Matsya/Resources
+sudo mv Scripts/* /opt/Matsya/Scripts
+
+sudo rm -rf /opt/Matsya/tmp/Udemy1
+
+sudo chmod -R 777 /opt/Matsya/Scripts
